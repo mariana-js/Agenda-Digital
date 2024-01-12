b5820b7504f139e740ead4114a94b7b66f652a17
Mariana Silva <marianadejesussilva921@gmail.com>
2024-01-12 16:17:47 -0300

    Add: Initial Code of the Project Angular Frontend.

---
+++ Frontend/agendadigital/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css',
+
+  template: `<h1>Hello world!</h1>`
+})
+export class AppComponent {
+  title = 'homes';
+
+  
+}
