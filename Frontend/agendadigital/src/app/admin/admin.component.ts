022a65000b1e7a2a9f5a6a6322b617691962d0ce
Mariana <marianadejesussilva921@gmail.com>
2024-01-22 22:29:59 -0300

    Add: Pages Admin initial configs.

--- Frontend/agendadigital/src/app/admin/admin.component.ts
+++ Frontend/agendadigital/src/app/admin/admin.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavAdminComponent } from '../nav-admin/nav-admin.component';
+import { PrincipalComponent } from "../principal/principal.component";
 
 @Component({
-  selector: 'app-admin',
-  standalone: true,
-  imports: [NavAdminComponent],
-  templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+    selector: 'app-admin',
+    standalone: true,
+    templateUrl: './admin.component.html',
+    styleUrl: './admin.component.css',
+    imports: [NavAdminComponent, PrincipalComponent]
 })
 export class AdminComponent {
 
