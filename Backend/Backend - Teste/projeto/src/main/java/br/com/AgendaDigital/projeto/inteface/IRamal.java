4fa7626c49ef50b248fa6f4ec9c51fd9e1499840
Mariana Silva <marianadejesussilva921@gmail.com>
2024-01-24 11:26:34 -0300

    Del: Old Backends and Alt: styles css in classes admin.

--- Backend/Backend - Teste/projeto/src/main/java/br/com/AgendaDigital/projeto/inteface/IRamal.java
+++
@@ -1,9 +0,0 @@
-package br.com.AgendaDigital.projeto.inteface;
-
-import org.springframework.data.repository.CrudRepository;
-
-import br.com.AgendaDigital.projeto.model.Ramal;
-
-public interface IRamal extends CrudRepository<Ramal, String>{
-
-}
