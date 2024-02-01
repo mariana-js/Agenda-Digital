b011420c62d7693182783e51c3fc165d6a16c0ba
Mariana <marianadejesussilva921@gmail.com>
2024-02-01 20:47:43 -0300

    Alt: Separate ramais and setores styles routes in project.

--- Frontend/agendadigital/src/app/admin/admin.component.ts
+++ Frontend/agendadigital/src/app/admin/admin.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { NavAdminComponent } from '../nav-admin/nav-admin.component';
 import { PrincipalComponent } from "../principal/principal.component";
+import { SetoresComponent } from "./setores/setores.component";
+import { RamaisComponent } from "./ramais/ramais.component";
 
 @Component({
     selector: 'app-admin',
     standalone: true,
     templateUrl: './admin.component.html',
     styleUrl: './admin.component.css',
-    imports: [NavAdminComponent, PrincipalComponent]
+    imports: [NavAdminComponent, PrincipalComponent, SetoresComponent, RamaisComponent]
 })
 export class AdminComponent {
 
