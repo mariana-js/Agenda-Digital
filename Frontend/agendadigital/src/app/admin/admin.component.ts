f81e39ddab849c3fd08817815a4531f8e13ad639
Mariana <marianadejesussilva921@gmail.com>
2024-02-01 21:55:59 -0300

    Alt: Styles page principal.

--- Frontend/agendadigital/src/app/admin/admin.component.ts
+++ Frontend/agendadigital/src/app/admin/admin.component.ts
@@ -4,13 +4,14 @@ import { PrincipalComponent } from "../principal/principal.component";
 import { SetoresComponent } from "./setores/setores.component";
 import { RamaisComponent } from "./ramais/ramais.component";
 import { UsuariosComponent } from "./usuarios/usuarios.component";
+import { ContatosHideComponent } from "./contatos-hide/contatos-hide.component";
 
 @Component({
     selector: 'app-admin',
     standalone: true,
     templateUrl: './admin.component.html',
     styleUrl: './admin.component.css',
-    imports: [NavAdminComponent, PrincipalComponent, SetoresComponent, RamaisComponent, UsuariosComponent]
+    imports: [NavAdminComponent, PrincipalComponent, SetoresComponent, RamaisComponent, UsuariosComponent, ContatosHideComponent]
 })
 export class AdminComponent {
 
