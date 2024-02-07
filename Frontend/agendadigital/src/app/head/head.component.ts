846c0b50eacf73717b5db02388d9756d3f5632c0
Mariana Silva <marianadejesussilva921@gmail.com>
2024-02-07 11:51:26 -0300

    ADD: Nav aniversariantes.

--- Frontend/agendadigital/src/app/head/head.component.ts
+++ Frontend/agendadigital/src/app/head/head.component.ts
@@ -3,13 +3,14 @@ import { AdminComponent } from "../admin/admin.component";
 import { PrincipalComponent } from "../principal/principal.component";
 import { NavAdminComponent } from "../nav-admin/nav-admin.component";
 import { SobreComponent } from "../sobre/sobre.component";
+import { NavAniversariantesComponent } from "../nav-aniversariantes/nav-aniversariantes.component";
 
 @Component({
     selector: 'app-head',
     standalone: true,
     templateUrl: './head.component.html',
     styleUrl: './head.component.css',
-    imports: [AdminComponent, PrincipalComponent, NavAdminComponent, SobreComponent]
+    imports: [AdminComponent, PrincipalComponent, NavAdminComponent, SobreComponent, NavAniversariantesComponent]
 })
 export class HeadComponent {
 
---
+++ Frontend/agendadigital/src/app/nav-aniversariantes/nav-aniversariantes.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-aniversariantes',
+  standalone: true,
+  imports: [],
+  templateUrl: './nav-aniversariantes.component.html',
+  styleUrl: './nav-aniversariantes.component.css'
+})
+export class NavAniversariantesComponent {
+
+}
