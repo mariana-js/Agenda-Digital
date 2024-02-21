cde7002df02762015f50c2c11fb90ada06b19ed6
Mariana Silva <marianadejesussilva921@gmail.com>
2024-02-21 08:42:39 -0300

    Add: Page Pessoa/Contato.

--- Frontend/agendadigital/src/app/head/head.component.ts
+++ Frontend/agendadigital/src/app/head/head.component.ts
@@ -4,13 +4,14 @@ import { PrincipalComponent } from "../principal/principal.component";
 import { NavAdminComponent } from "../nav-admin/nav-admin.component";
 import { SobreComponent } from "../sobre/sobre.component";
 import { NavAniversariantesComponent } from "../nav-aniversariantes/nav-aniversariantes.component";
+import { PessoaComponent } from "../principal/pessoa/pessoa.component";
 
 @Component({
     selector: 'app-head',
     standalone: true,
     templateUrl: './head.component.html',
     styleUrl: './head.component.css',
-    imports: [AdminComponent, PrincipalComponent, NavAdminComponent, SobreComponent, NavAniversariantesComponent]
+    imports: [AdminComponent, PrincipalComponent, NavAdminComponent, SobreComponent, NavAniversariantesComponent, PessoaComponent]
 })
 export class HeadComponent {
 
