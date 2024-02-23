eb7daa57f955d3d53505f47216ecf59818689c3d
Mariana Silva <marianadejesussilva921@gmail.com>
2024-02-23 11:50:59 -0300

    ADD: Styles finaly page pessoa/contact.

--- Frontend/agendadigital/src/app/app.module.ts
+++ Frontend/agendadigital/src/app/app.module.ts
@@ -3,9 +3,9 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
-import { Router } from '@angular/router';
+
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
