ba7c8832ff4ba5d8df4d341bf9a082517f47accf
Mariana Silva <marianadejesussilva921@gmail.com>
2024-02-26 11:59:04 -0300

    OK: Routes Head.

--- Frontend/agendadigital/src/app/head/head.component.ts
+++ Frontend/agendadigital/src/app/head/head.component.ts
@@ -1,23 +1,25 @@
 import { Component } from '@angular/core';
-import { AdminComponent } from "../admin/admin.component";
-import { NavAdminComponent } from "../nav-admin/nav-admin.component";
-import { NavAniversariantesComponent } from "../nav-aniversariantes/nav-aniversariantes.component";
-import { PessoaComponent } from "../principal/pessoa/pessoa.component";
-import { PrincipalComponent } from "../principal/principal.component";
-import { SobreComponent } from "../sobre/sobre.component";
 import { Router } from '@angular/router';
-
 @Component({
-    selector: 'app-head',
-    standalone: true,
-    templateUrl: './head.component.html',
-    styleUrl: './head.component.css',
-    imports: [AdminComponent, PrincipalComponent, NavAdminComponent, SobreComponent, NavAniversariantesComponent, PessoaComponent]
+  selector: 'app-head',
+  standalone: true,
+  templateUrl: './head.component.html',
+  styleUrl: './head.component.css',
+  imports: [],
 })
-export class HeadComponent {
-  constructor(private router: Router) {}
 
-  navegarParaAbout() {
-    this.router.navigate(['/principal']);
+export class HeadComponent {
+  constructor(private router: Router) { }
+
+  navegarParaHome() {
+    this.router.navigate(['/']);
+  }
+
+  navegarParaAdmin() {
+    this.router.navigate(['/ramais']);
+  }
+
+  navegarParaSobre() {
+    this.router.navigate(['/sobre']);
   }
 }
--- Frontend/agendadigital/src/app/principal/principal.component.ts
+++ Frontend/agendadigital/src/app/principal/principal.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { NavAniversariantesComponent } from "../nav-aniversariantes/nav-aniversariantes.component";
+import { CadatrarContatoComponent } from '../admin/cadatrar-contato/cadatrar-contato.component';
+import { SetoresComponent } from '../admin/setores/setores.component';
 
 @Component({
     selector: 'app-principal',
     standalone: true,
     templateUrl: './principal.component.html',
     styleUrl: './principal.component.css',
-    imports: [ NavAniversariantesComponent]
+    imports: [ NavAniversariantesComponent,CadatrarContatoComponent,SetoresComponent]
 })
 export class PrincipalComponent{
     constructor(private router: Router) {}
 
   navegarParaAbout() {
     this.router.navigate(['/']);
+    this.router.navigate(['/setores']);
+    this.router.navigate(['/cadastrar-contato']);
+    console.log('navegando...')
   }
 }
