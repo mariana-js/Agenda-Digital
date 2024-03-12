f112714dc56a08514a83068571b1a7d7dbc47a55
Mariana Silva <marianadejesussilva921@gmail.com>
2024-03-12 09:56:07 -0300

    ADD: Get Setores in page Setores Admin.

--- Frontend/agendadigital/src/app/admin/setores/setores.component.ts
+++ Frontend/agendadigital/src/app/admin/setores/setores.component.ts
@@ -1,12 +1,31 @@
+import { NgFor } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { Setor } from '../../models/setor';
 import { NavAdminComponent } from "../nav-admin/nav-admin.component";
 @Component({
   selector: 'app-setores',
   standalone: true,
   templateUrl: './setores.component.html',
   styleUrl: './setores.component.css',
-  imports: [NavAdminComponent]
+  imports: [NavAdminComponent, NgFor, HttpClientModule]
 })
 export class SetoresComponent {
+  readonly url: string;
+  setores: Setor[] = [];
 
+
+  constructor(private http: HttpClient) {
+    this.url = 'http://localhost:8080';
+  }
+  ngOnInit() {
+    this.getSetores();
+  }
+
+  getSetores() {
+    this.http.get<Setor[]>(`${this.url}/setor`)
+      .subscribe(resultados => {
+        this.setores = resultados;
+      });
+  }
 }
