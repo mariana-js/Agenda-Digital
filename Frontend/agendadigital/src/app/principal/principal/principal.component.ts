0be0d90ed8430d2bad2c95101337d00258dfd6a7
Mariana Silva <marianadejesussilva921@gmail.com>
2025-05-29 12:12:22 -0300

    ADD: Reolvido o problema do standalone e component Principal ts refatorado!

--- Frontend/agendadigital/src/app/principal/principal/principal.component.ts
+++ Frontend/agendadigital/src/app/principal/principal/principal.component.ts
@@ -1,38 +1,37 @@
-import { NgClass, NgFor, NgIf, NgStyle } from '@angular/common';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { CommonModule, NgFor, NgIf, NgStyle } from '@angular/common';
+import { HttpClientModule } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Contato } from '../../models/contato';
 import { ContatoStateService } from '../../services/contato-state.service';
 import { NavAniversariantesComponent } from "../nav-aniversariantes/nav-aniversariantes.component";
-import { forkJoin } from 'rxjs';
+import { PessoaService } from './../../services/pessoa.service';
 @Component({
   selector: 'app-principal',
   standalone: true,
   templateUrl: './principal.component.html',
   styleUrl: './principal.component.css',
-  imports: [HttpClientModule, NavAniversariantesComponent, NgFor, FormsModule, NgIf, NgStyle,NgClass]
+  imports: [CommonModule, HttpClientModule, NavAniversariantesComponent, NgFor, FormsModule, NgIf, NgStyle]
 })
 export class PrincipalComponent {
-  readonly url: string
+  // readonly url: string
 
   contatos: Contato[] = [];
   id_contatoSelecionado: string | null = null;
   searchTerm: string = '';
   retorno: string = "";
   amount: number = 0;
-  itemsPerPage = 15;
+  itemsPerPage = 8;
   currentPage = 1;
 
   get totalPages(): number {
     return Math.ceil(this.contatos.length / this.itemsPerPage);
   }
-   constructor(
-    private http: HttpClient,
-    private router: Router,
-    private contatoStateService: ContatoStateService) {
-    this.url = 'http://localhost:8080';
+  constructor(
+    private readonly router: Router,
+    private readonly contatoStateService: ContatoStateService,
+    private readonly pessoaService: PessoaService) {
   }
   nextPage() {
     if (this.currentPage < this.totalPages) {
@@ -55,29 +54,54 @@ export class PrincipalComponent {
     }
   }
   ngOnInit() {
+    const savedState = this.contatoStateService.getState();
 
-    this.getContatos();
-
-  }
-  getContatos() {
-    this.http.get<Contato[]>(`${this.url}/pessoa`)
-      .subscribe(resultados => {
-        this.contatos = resultados.filter(contato => contato.flag_privado === false);
-        this.contatos.sort((a, b) => a.nome_pessoa.localeCompare(b.nome_pessoa));
-        this.amount = this.contatos.length;
-        if (this.amount === 0) {
-          console.log("Erro ao trazer os contatos!");
+    if (savedState) {
+      this.currentPage = savedState.currentPage || 1;
+      this.searchTerm = savedState.searchTerm || '';
+      this.getContatos(() => {
+        if (this.searchTerm) {
+          this.filterContacts(this.searchTerm);
         }
-      })
-  }
-  getFuncionarios() {
-    this.http.get<Contato[]>(`${this.url}/pessoa`)
-      .subscribe(resultados => {
-        this.contatos = resultados.filter(contatos => contatos.flag_funcionario === true);
-        this.amount = this.contatos.length;
-        this.contatos.sort((a, b) => a.nome_pessoa.localeCompare(b.nome_pessoa));
+        setTimeout(() => window.scrollTo(0, savedState.scrollY || 0), 0);
       });
+    } else {
+      this.getContatos();
+    }
+
+    this.contatoStateService.clearContatoSelecionado();
+    this.contatoStateService.saveState(null);
+
+  } getContatos(callback?: () => void) {
+    this.pessoaService.getPessoa().subscribe(r => {
+      this.contatos = r.filter(r => r.flag_privado === false);
+      this.contatos.sort((a, b) => a.nome_pessoa.localeCompare(b.nome_pessoa));
+      this.amount = this.contatos.length;
+      if (this.amount === 0) {
+        console.log("Erro ao trazer os contatos!");
+      }
+
+      if (callback) {
+        callback();
+      }
+    });
+  } getFuncionarios() {
+    this.pessoaService.getPessoa().subscribe(r => {
+      this.contatos = r.filter(contatos => contatos.flag_funcionario === true);
+      this.contatos.sort((a, b) => a.nome_pessoa.localeCompare(b.nome_pessoa));
+      this.amount = this.contatos.length;
+      if (this.amount === 0) {
+        console.log("Erro ao trazer os contatos!");
+      }
+    });
   } informacoes(contato: Contato) {
+    // Salvando o estado atual da página e do scrolly:
+    this.contatoStateService.saveState({
+      currentPage: this.currentPage,
+      searchTerm: this.searchTerm,
+      scrollY: window.scrollY
+    });
+
     contato.id_contatoSelecionado = contato.id_pessoa;
     this.contatoStateService.contatoSelecionado = contato;
     this.router.navigate(['/contato', contato.id_contatoSelecionado]);
@@ -91,17 +115,17 @@ export class PrincipalComponent {
     } else {
       this.retorno = "";
     }
-    this.http.get<Contato[]>(`${this.url}/pessoa`)
-      .subscribe(resultados => {
-        this.contatos = resultados.filter(contato =>
-          contato.flag_privado === false &&
-          contato.nome_pessoa.toLowerCase().includes(searchTerm.toLowerCase())
-        );
-        this.amount = this.contatos.length;
-        if (this.amount === 0) {
-          this.retorno = "Nenhum contato encontrado.";
-          this.getContatos();
-        }
-      });
+    const contatosFiltrados = this.contatos.filter(contato =>
+      contato.nome_pessoa.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+
+    if (contatosFiltrados) {
+      this.contatos = contatosFiltrados;
+      this.amount = this.contatos.length;
+    } else if (this.amount === 0) {
+      this.retorno = "Nenhum contato encontrado.";
+      this.getContatos();
+    }
   }
 }
+
