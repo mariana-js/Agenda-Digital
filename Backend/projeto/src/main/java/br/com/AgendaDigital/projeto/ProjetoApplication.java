e8e1899b9aae15fc75c1cff5f73009ba5979ef30
Mariana <marianadejesussilva921@gmail.com>
2025-06-22 15:08:49 -0300

    Some changes inf Front

--- Backend/projeto/src/main/java/br/com/AgendaDigital/projeto/ProjetoApplication.java
+++ Backend/projeto/src/main/java/br/com/AgendaDigital/projeto/ProjetoApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ProjetoApplication {
-	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetoApplication.class, args);
 	}
